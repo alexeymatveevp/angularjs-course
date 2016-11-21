6c88cb0df46d19a7f8975955a880d27920dc8100
Alexey.Matveev <Alexey.Matveev@t-systems.ru>
2016-11-21 20:05:57 +0300

    upd

--- test.js
+++ test.js
@@ -1,1 +1,10 @@
-console.log('the quick brown fox...');
+function getData(callback) {
+    console.log('long process start...');
+    setTimeout(function() {
+        console.log('...long process end');
+        callback();
+    }, 2000);
+}
+getData(function(data) {
+    console.log('The data is: ' + data);
+});
