51a431988df36f4e55088bc37eabdf35249c5d50
Alexey <david.lucky.star@gmail.com>
2016-12-09 17:19:37 +0300

    lection 3 sucks

---
+++ apps/2ngfollowup/flow.js
@@ -0,0 +1,32 @@
+//angular.bootstrap(document);
+
+angular.module('app', []);
+//angular.module('app', []);
+angular.module('app').controller('ctrl', function($scope, UnicornService) {
+    var vm = this;
+
+    this.asdf = 'fdsa';
+    this.click = function() {
+        console.log($scope)
+        this.asdf = 'asdf'
+    }
+    this.data = {
+        www: 'www'
+    };
+    $scope.$watch('ct.data.www', function(val, old) {
+        console.log(val)
+
+        console.log(old)
+    })
+
+    console.log($scope)
+
+
+    $scope.$watchGroup(['ct.toEvaluate', 'ct.data.www'], function(val) {
+        vm.evaluated = $scope.$eval(val[0]);
+        vm.data.www = vm.data.www+"a";
+    })
+});
+
+
+
---
+++ apps/2ngfollowup/services/UnicornService.js
@@ -0,0 +1,10 @@
+angular.module('app').service('UnicornService', function() {
+    this.sayHi = function() {
+        return 'HI';
+    }
+});
+
+
+angular.module('app').factory('UnicornFactory', function() {
+
+});
